a9ea1fa08101909bb735b2b0b200d472402e85b0
akhrullo <a.ibrokhimov@uzum.com>
2024-10-04 17:57:31 +0500

    feat: optimized front pages

--- src/main/java/com/akhrullo/webchat/controller/MainController.java
+++ src/main/java/com/akhrullo/webchat/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AuthModelController {
+public class MainController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
